ea417edc6894049e5b9c56ad00efb58014e31b0d
acedia503 <acedia503@gmail.com>
2026-03-10 19:31:33 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
 from storage import init_db
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
